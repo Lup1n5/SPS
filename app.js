cc9b184a69fdfc44678a1892ca2f3240fb5b0e84
Lup1n5 <146459441+Lup1n5@users.noreply.github.com>
2025-01-17 16:04:03 +0000

    e

--- app.js
+++ app.js
@@ -7,7 +7,13 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+import { getAuth, 
+  createUserWithEmailAndPassword, 
+  signInWithEmailAndPassword, 
+  GoogleAuthProvider,
+  signInWithPopup,
+  onAuthStateChanged,
+  signOut } from "firebase/auth";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAYjLbsdGgVccTHa_bpEaDh7orYmzldiMk",
@@ -21,7 +27,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
+const auth = getAuth();
 
 ws.onmessage = (event) => {
   console.log('Message received from server')
@@ -41,7 +47,130 @@ const clearChatBtn = document.querySelector('.clear-chat-button')
 
 let messageSender = ''
 let chatCode = ''
+// logged in and logged out sections
+const loggedInView = document.getElementById('logged-in-view')
+const loggedOutView = document.getElementById('logged-out-view')
+const userEmail = document.getElementById('user-email')
 
+// email and password for signin
+const emailSignInForm = document.getElementById('signin-email-input')
+const passwordSignInForm = document.getElementById('signin-password-input')
+
+// email and password for signup
+const emailSignUpForm = document.getElementById('signup-email-input')
+const passwordSignUpForm = document.getElementById('signup-password-input')
+
+// Buttons
+const signInGoogleBtn = document.getElementById('sign-in-with-google-btn')
+const signUpGoogleBtn = document.getElementById('sign-up-with-google-btn')
+const googleBtns = [signInGoogleBtn, signUpGoogleBtn]
+
+const createAccountBtn = document.getElementById('sign-up-btn')
+const loginBtn = document.getElementById('sign-in-btn')
+const logoutBtn = document.getElementById('logout-button')
+
+
+// Detects state change
+onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/auth.user
+      const uid = user.uid;
+      const email = user.email
+      loggedInView.style.display = 'block'
+      userEmail.innerText = email
+      loggedOutView.style.display = 'none'
+      // ...
+    } else {
+      // User is signed out
+      // ...
+      loggedInView.style.display = 'none'
+      loggedOutView.style.display = 'block'
+    }
+  });
+
+
+// Event Listeners for Buttons
+// Click on Create Account Button
+createAccountBtn.addEventListener('click', () => {
+    createUserWithEmailAndPassword(auth, emailSignUpForm.value, passwordSignUpForm.value)
+        .then((userCredential) => {
+            // Signed up 
+            const user = userCredential.user;
+            console.log(user)
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorMessage)
+            // ..
+        });
+    console.log('Create Account Button Clicked')
+    console.log(`Email: ${emailSignUpForm.value}`)
+    console.log(`Password: ${passwordSignUpForm.value}`)
+})
+
+// Click on Login Button
+loginBtn.addEventListener('click', () => {
+    signInWithEmailAndPassword(auth, emailSignInForm.value, passwordSignInForm.value)
+        .then((userCredential) => {
+            // Signed in 
+            const user = userCredential.user;
+            console.log(user)
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorMessage)
+        });
+
+    console.log('Login Clicked')
+    console.log(`Email: ${emailSignInForm.value}`)
+    console.log(`Password: ${passwordSignInForm.value}`)
+})
+
+// Click on Google Signin
+googleBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential.accessToken;
+                // The signed-in user info.
+                const user = result.user;
+                console.log(user);
+                // IdP data available using getAdditionalUserInfo(result)
+                // ...
+            }).catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorMessage);
+                // The email of the user's account used.
+                const email = error.customData.email;
+                // The AuthCredential type that was used.
+                const credential = GoogleAuthProvider.credentialFromError(error);
+                // ...
+            });
+        console.log('Google Signin Clicked');
+    });
+});
+
+
+
+// logout button
+logoutBtn.addEventListener('click', () => {
+    signOut(auth).then(() => {
+        // Sign-out successful.
+      }).catch((error) => {
+        // An error happened.
+      });
+      
+    console.log('Logout Clicked')
+})
 const createChatMessageElement = (message) => `
   <div class="message ${message.sender === messageSender ? 'blue-bg' : 'gray-bg'}">
     <div class="message-sender">${message.sender}</div>
