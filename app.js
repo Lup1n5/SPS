4965aade4cac2f173e0f86263ad23e8564931647
Lup1n5 <146459441+Lup1n5@users.noreply.github.com>
2025-01-21 13:59:36 +0000

    hello

--- app.js
+++ app.js
@@ -5,6 +5,8 @@
 
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+//heloo
+console.log("difference");
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth, 
