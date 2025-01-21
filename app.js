afec4361c90c2e857e8beedb20f5c2758828a7a9
Lup1n5 <146459441+Lup1n5@users.noreply.github.com>
2025-01-21 13:52:50 +0000

    x

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
 //const ws = new WebSocket('ws://localhost:3000/server')
 
 // Import the functions you need from the SDKs you need
-//import { initializeApp } from "https://www.gstatic.com/firebasejs/11.1.0/firebase-app.js";
+import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth, 
